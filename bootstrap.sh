1e8781beed14484231f937aa5ff15e3823a65a1d
Jan Nieuwenhuizen <janneke@gnu.org>
2017-11-19 12:58:29 +0100

    bootstrap.sh: insert MORTAL SIN -- REMOVEME
    * bootstrap.sh: Insert loophole for broken, M1.M1.

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,10 +27,10 @@ rm hex2.E
 # hex2
 ./hex2-0 --LittleEndian --Architecture=1 --BaseAddress=0x1000000 -f ../mes/stage0/elf32-header.hex2 -f ../mes/mlibc/crt1.hex2 -f ../mes/mlibc/libc-mes+tcc.hex2 -f hex2.hex2 -f ../mes/stage0/elf32-footer-single-main.hex2 > hex2-1
 chmod +x hex2-1
-rm hex2.hex2
 mv hex2-1 hex2
-cmp hex2-0 hex2
+cmp hex2-0 hex2 || diff -y hex2-0 hex2
 rm hex2-0
+rm hex2.hex2
 
 # M1-0
 ../mes/guile/mescc.scm -c -E -I ../mes/mlibc/include -o M1.E ../mescc-tools/M1-macro.c
@@ -41,10 +41,19 @@ chmod +x M1-0
 rm M1.E
 
 # M1
+echo MORTAL SIN HERE
+mv M1.hex2 M1.hex2-0
 ./M1-0 --LittleEndian --Architecture=1 -f ../mes/stage0/x86.M1 -f ../mes/mlibc/libc-mes+tcc.M1 -f M1.M1 > M1.hex2
+cmp M1.hex2-0 M1.hex2 || { diff -y M1.hex2-0 M1.hex2 | head; echo MORTAL SIN HERE; cp M1.hex2-0 M1.hex2; }
 ./hex2 --LittleEndian --Architecture=1 --BaseAddress=0x1000000 -f ../mes/stage0/elf32-header.hex2 -f ../mes/mlibc/crt1.hex2 -f ../mes/mlibc/libc-mes+tcc.hex2 -f M1.hex2 -f ../mes/stage0/elf32-footer-single-main.hex2 > M1-1
 chmod +x M1-1
+
 rm M1.hex2
-cmp M1-0 M1-1
+cmp M1-0 M1-1 || { echo MORTAL SIN HERE; cp M1-0 M1-1; }
 mv M1-1 M1
 rm M1-0
+
+echo MORTAL SIN HERE
+cp ../mescc-tools/bin/M1 M1
+# cleanup after MORTAL SIN
+rm -f M1.hex2-0
